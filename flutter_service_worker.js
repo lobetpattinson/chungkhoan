f53afefea2d70ec3fe15915bd93da078cb597dbf
kiethn <nguyenhamkiet2201@gmail.com>
2021-09-16 20:14:47 +0700

    UPDATE

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -24,9 +24,9 @@ const RESOURCES = {
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"index.html": "6f007c74b74fe3b3cc29a996a9d3939c",
-"/": "6f007c74b74fe3b3cc29a996a9d3939c",
-"main.dart.js": "ae08be685970f1f73bd886073294ad9f",
+"index.html": "6ec8418c1b6aa6e9b025511ed24f9f38",
+"/": "6ec8418c1b6aa6e9b025511ed24f9f38",
+"main.dart.js": "ea559214fd2592bc2c81848b776c5b46",
 "manifest.json": "324654d40e6c5cc1344cd8583299bd76",
 "version.json": "cbdbd28247e8fb5f876bbf11ca852cb4"
 };
